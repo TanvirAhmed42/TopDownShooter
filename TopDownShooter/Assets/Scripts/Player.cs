1bb72758e4486b8ce50ce43081a056c8b34c491d
TanvirAhmed42 <43964962+TanvirAhmed42@users.noreply.github.com>
2018-10-19 19:40:50 +0600

    Added a gun system
    So the player can shoot now by holding down left mouse button. The gun is made up of cubes. The bullets are also cubes. Different bullets can have different velocity and delay between shots. Different gun may use different bullets. Right now the bullets cluster up the hierarchy but it will be fixed soon.

--- TopDownShooter/Assets/Scripts/Player.cs
+++ TopDownShooter/Assets/Scripts/Player.cs
@@ -3,23 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent (typeof (PlayerController))]
+[RequireComponent (typeof (GunController))]
 public class Player : MonoBehaviour {
 
 	public float moveSpeed = 5f;
 
 	Camera mainCam;
 	PlayerController controller;
+	GunController gunController;
 
 	void Start () {
 		mainCam = Camera.main;
 		controller = GetComponent<PlayerController> ();
+		gunController = GetComponent<GunController> ();
 	}
 
 	void Update () {
+		//Move Input
 		Vector3 moveInput = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0f, Input.GetAxisRaw ("Vertical"));
 		Vector3 moveVelocity = moveInput.normalized * moveSpeed;
 		controller.Move (moveVelocity);
 
+		//Look Input
 		Ray ray = mainCam.ScreenPointToRay (Input.mousePosition);
 		Plane groundPlane = new Plane (Vector3.up, Vector3.zero);
 		float rayDistance;
@@ -28,6 +33,11 @@ public class Player : MonoBehaviour {
 			Vector3 point = ray.GetPoint (rayDistance);
 			//Debug.DrawLine (ray.origin, point, Color.red);
 			controller.LookAt (point);
+		}
+
+		//Weapon Input
+		if (Input.GetMouseButton (0)) {
+			gunController.Shoot ();
 		}
 	}
 }
---
+++ TopDownShooter/Assets/Scripts/Projectile.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+	float speed = 10f;
+
+	void Update () {
+		transform.Translate (Vector3.forward * speed * Time.deltaTime);
+	}
+
+	public void SetSpeed (float _speed) {
+		speed = _speed;
+	}
+}
