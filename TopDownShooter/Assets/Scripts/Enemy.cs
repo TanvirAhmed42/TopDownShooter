5172ee7b2a8bbe98a9d77b7e6addc683321d79e5
TanvirAhmed42 <43964962+TanvirAhmed42@users.noreply.github.com>
2018-10-19 20:15:49 +0600

    Added an enemy
    Added in another capsule as enemy. The enemy has a navmesh agent attached to it. So it can chase around the player. Bullets can now collide with enemy. Enemy's capsule collider is set to trigger so that it can't throw the player off the ground or push him around. To improve performance navmesh agent updates only four times a second.

---
+++ TopDownShooter/Assets/Scripts/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent (typeof (NavMeshAgent))]
+public class Enemy : MonoBehaviour {
+
+	NavMeshAgent agent;
+	Transform target;
+
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+		target = GameObject.FindGameObjectWithTag ("Player").transform;
+
+		StartCoroutine (UpdatePath ());
+	}
+
+	void Update () {
+	}
+
+	IEnumerator UpdatePath () {
+		float refreshRate = .25f;
+
+		while (target != null) {
+			Vector3 targetPosition = new Vector3 (target.position.x, 0f, target.position.z);
+			agent.SetDestination (targetPosition);
+
+			yield return new WaitForSeconds (refreshRate);
+		}
+	}
+}
--- TopDownShooter/Assets/Scripts/Projectile.cs
+++ TopDownShooter/Assets/Scripts/Projectile.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour {
 
+	public LayerMask collisionMask;
+
 	float speed = 10f;
 
 	void Update () {
-		transform.Translate (Vector3.forward * speed * Time.deltaTime);
+		float moveDistance = speed * Time.deltaTime;
+		CheckCollisions (moveDistance);
+		transform.Translate (Vector3.forward * moveDistance);
 	}
 
 	public void SetSpeed (float _speed) {
 		speed = _speed;
+	}
+
+	void CheckCollisions (float moveDistance) {
+		Ray ray = new Ray (transform.position, transform.forward);
+		RaycastHit hit;
+
+		if (Physics.Raycast (ray, out hit, moveDistance, collisionMask, QueryTriggerInteraction.Collide)) {
+			OnHitObject (hit);
+		}
+	}
+
+	void OnHitObject (RaycastHit hit) {
+		Debug.Log (hit.collider.name);
+		Destroy (gameObject);
 	}
 }
