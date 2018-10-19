b72c82a8d294662c1fcca6358e0cf73f113785a7
TanvirAhmed42 <43964962+TanvirAhmed42@users.noreply.github.com>
2018-10-19 18:52:30 +0600

    Added in the player controller
    The player controller is ready. The player can move around in the ground. It uses a rigidbody to detect collision. Also player looks at the mouse position. Here I'm using a default capsule as the player. Player movement might need to smooth a bit.

---
+++ TopDownShooter/Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (PlayerController))]
+public class Player : MonoBehaviour {
+
+	public float moveSpeed = 5f;
+
+	Camera mainCam;
+	PlayerController controller;
+
+	void Start () {
+		mainCam = Camera.main;
+		controller = GetComponent<PlayerController> ();
+	}
+
+	void Update () {
+		Vector3 moveInput = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0f, Input.GetAxisRaw ("Vertical"));
+		Vector3 moveVelocity = moveInput.normalized * moveSpeed;
+		controller.Move (moveVelocity);
+
+		Ray ray = mainCam.ScreenPointToRay (Input.mousePosition);
+		Plane groundPlane = new Plane (Vector3.up, Vector3.zero);
+		float rayDistance;
+
+		if (groundPlane.Raycast (ray, out rayDistance)) {
+			Vector3 point = ray.GetPoint (rayDistance);
+			//Debug.DrawLine (ray.origin, point, Color.red);
+			controller.LookAt (point);
+		}
+	}
+}
---
+++ TopDownShooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Rigidbody))]
+public class PlayerController : MonoBehaviour {
+
+	Rigidbody myRigidbody;
+
+	Vector3 velocity;
+
+	void Start () {
+		myRigidbody = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate () {
+		myRigidbody.MovePosition (myRigidbody.position + velocity * Time.fixedDeltaTime);
+	}
+
+	public void Move (Vector3 _velocity) {
+		velocity = _velocity;
+	}
+
+	public void LookAt (Vector3 lookPoint) {
+		Vector3 heightCorrectedPoint = new Vector3 (lookPoint.x, transform.position.y, lookPoint.z);
+		transform.LookAt (heightCorrectedPoint);
+	}
+}
